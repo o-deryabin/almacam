b7de7f2dc2b444bf931d0cdbf612b3e3ad3d3836
Oleg Deryabin <olegderyabin22@gmail.com>
2021-10-04 13:16:54 +0600

    MD 04102021 1316

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,11 @@
 const { Router } = require("express");
-// const mailer = require("../nodemailer");
 const User = require("../models/User");
 
 const router = Router();
 
 router.post("/send", async (req, res) => {
   try {
-    if (!req.body.firstname || !req.body.tel) {
+    if (!req.body.site || !req.body.firstname || !req.body.tel) {
       return res.status(400).json({ message: "Не все данные указаны" });
     }
 
@@ -16,22 +15,10 @@ router.post("/send", async (req, res) => {
 
     await user.save();
 
-    // const message = {
-    //   to: "olegderyabin22@gmail.com, vassilyev.com@gmail.com", // list of receivers
-    //   subject: "Новая заявка!!!", // Subject line
-    //   text: `Данные человека
-
-    //   Имя: ${firstname}
-    //   Телефон: ${tel}
-    //   Дата заявки: ${new Date().toLocaleDateString()}`, // plain text body
-    // };
-
-    // mailer(message);
-
     res.status(200).json({ message: "Заявка отправлена" });
   } catch (e) {
     console.log(e);
-    res.status(500).json({ message: "что-то пошло не так" });
+    res.status(500).json({ message: "Что-то пошло не так" });
   }
 });
 
